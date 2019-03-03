9689517be70545b586d760306841db51d768a1ab
Joao <joaobernardopa@gmail.com>
2019-03-03 12:52:31 +0000

    working with images

--- src/com/joao/MyOwnServer/MyServer.java
+++ src/com/joao/MyOwnServer/MyServer.java
@@ -15,6 +15,8 @@ public class MyServer {
     private String fileRequested;
     private PrintWriter out;
     private BufferedOutputStream outData;
+    private PrintWriter out_img;
+    private BufferedOutputStream outData_img;
 
 
     public static void main(String[] args){
@@ -63,12 +65,24 @@ public class MyServer {
          int fileLength = (int) file.length();
          byte[] fileData = new byte[fileLength];
 
+         //image file
+        File fileImg = new File("www/lin.png");
+        int fileImgLength = (int) fileImg.length();
+        byte[] fileImgData = new byte[fileImgLength];
+
          if(method.equals("GET")){
              try{
                  FileInputStream fileStream = new FileInputStream(file);
                  fileStream.read(fileData);
                  out = new PrintWriter(socket.getOutputStream());
                  outData = new BufferedOutputStream(socket.getOutputStream());
+
+                 //image content
+                 FileInputStream fileImgStr = new FileInputStream(fileImg);
+                 fileImgStr.read(fileImgData);
+                 out_img = new PrintWriter(socket.getOutputStream());
+                 outData_img = new BufferedOutputStream(socket.getOutputStream());
+
              }
              catch(Exception e){
                  e.printStackTrace();
@@ -85,6 +99,21 @@ public class MyServer {
              try {
                  outData.write(fileData,0,fileLength);
                  outData.flush();
+             }
+             catch(IOException e){
+                 e.printStackTrace();
+             }
+
+             out_img.println("HTTP/1.1 200 OK");
+             out_img.println("Server: Java HTTP Server");
+             out_img.println("Content-type: " + "image/png");
+             out_img.println("Content-length: " + fileImgLength);
+             out_img.println();
+             out_img.flush();
+
+             try {
+                 outData_img.write(fileImgData,0,fileImgLength);
+                 outData_img.flush();
              }
              catch(IOException e){
                  e.printStackTrace();
